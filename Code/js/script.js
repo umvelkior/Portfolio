5ec8c57226ff18259be7ad7a905b3094699599f8
umvelkior <bareilhe@et.esiea.fr>
2025-02-09 15:51:59 +0100

    changement menu pour une interface plus adapté au format mobile

--- Code/js/script.js
+++ Code/js/script.js
@@ -1,7 +1,11 @@
 function openMenu() {
     document.getElementById("menu").style.display = "flex";
+    /*document.getElementById("openbtn").style.display = "none";*/
+    document.getElementById("openbtn").style.color = "white";
 }
 
 function closeMenu() {
     document.getElementById("menu").style.display = "none";
+    /*document.getElementById("openbtn").style.display = "flex";*/
+    document.getElementById("openbtn").style.color = "#004add";
 }
